f171178e2811d2b7dc757962e5dae7c30faae05f
stephanie-clark <szuroff@gmail.com>
2017-07-21 15:04:06 -0400

    add global admin styles

---
+++ admin-styles.php
@@ -0,0 +1,57 @@
+<style type="text/css">
+    
+	.wrap *{
+		box-sizing:border-box;
+	}
+
+	.wrap{
+		padding:12px;
+		box-sizing: border-box;
+	}
+
+	.wrap h1.wp-heading-inline {
+	    background: #ce1025;
+	    color: #fff;
+	    width: 100%;
+	    display: block;
+	    margin: 10px 0 15px 0;
+	    font-size: 15px;
+	    background-image: url(<?php echo plugins_url('/assets/logo.png', __FILE__) ?>);
+	    background-repeat: no-repeat;
+	    background-position: 20px center;
+	    background-size: auto 20px;
+	    line-height: 60px;
+	    position: relative;
+	    padding: 0px 0px 0px 150px;
+	}
+
+	h1.wp-heading-inline:before {
+	    content: '';
+	    width: 1px;
+	    height: 30px;
+	    background: rgba(255,255,255,.4);
+	    position: absolute;
+	    left: 135px;
+	    top: 15px;
+    }
+    
+    .ss-panel {
+        background: #fff;
+        box-sizing: border-box;
+        margin: 15px 0 30px 0;
+        position: relative;
+        box-shadow: 3px 3px 10px rgba(0,0,0,.1);
+        padding: 40px;
+        float: left;
+        border-left: 10px solid #ccc;
+    }
+
+    .ss-panel h3 {
+        font-size: 23px;
+        font-weight: 400;
+        margin-top: 0;
+    }
+
+
+</style>
+
